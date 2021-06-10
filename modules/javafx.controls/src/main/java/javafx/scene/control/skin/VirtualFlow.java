497e1c2c92be4eef6a13284525c4e9b7dd8660d7
xardif <xardif@gmail.com>
2021-06-10 15:50:26 +0200

    Add workaround for issues after 8089589 - NPE after calling setCellCount without cell factory

--- modules/javafx.controls/src/main/java/javafx/scene/control/skin/VirtualFlow.java
+++ modules/javafx.controls/src/main/java/javafx/scene/control/skin/VirtualFlow.java
@@ -2987,6 +2987,11 @@ private double adjustByPixelAmount(double numPixels) {
     }
 
     private void recalculateAndImproveEstimatedSize(int improve) {
+        final Callback<VirtualFlow<T>,T> currentCellFactory = getCellFactory();
+        if (currentCellFactory == null) {
+            return;
+        }
+
         int itemCount = getCellCount();
         int cacheCount = itemSizeCache.size();
         int added = 0;
